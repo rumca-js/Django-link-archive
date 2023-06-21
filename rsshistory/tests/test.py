16de8a98541f04906402e78d91a8ea61f050fe51
X Y Z <rozbujnik@gmail.com>
2023-06-21 08:58:11 +0000

    login form has header now

--- rsshistory/tests/test.py
+++ rsshistory/tests/test.py
@@ -1,12 +1,6 @@
 from django.test import TestCase
 from django.utils import timezone
 
-# from django.core.urlresolvers import reverse
-
-# from whatever.models import Whatever
-# from whatever.forms import WhateverForm
-
-
 # models test
 class ModelTest(TestCase):
     def test_model(self):
