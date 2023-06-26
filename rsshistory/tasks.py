c3edcd0d10b0a7ad795bea8c1da5bb75116f4122
X Y Z <rozbujnik@gmail.com>
2023-06-26 13:14:23 +0000

    Added missing tasks

---
+++ rsshistory/tasks.py
@@ -0,0 +1,32 @@
+from celery import shared_task
+from celery.utils.log import get_task_logger
+
+from linklibrary.celery import app, logger
+
+
+def subs_checker_task(arg):
+    """!
+    Checks for new entries in sources
+    """
+    logger.info("Refreshing sources")
+
+    from .threadhandlers import RefreshThreadHandler
+
+    handler = RefreshThreadHandler()
+    handler.refresh()
+
+    logger.info("Refreshing sources done")
+
+
+def subs_processing_task(arg):
+    """!
+    Processes jobs
+    """
+    logger.info("Processing source")
+
+    from .threadhandlers import HandlerManager
+
+    mgr = HandlerManager()
+    mgr.process_all()
+
+    logger.info("Processing done")
