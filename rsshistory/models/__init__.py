ad6cfafad197adfac35d96243cd7c2c36d9c0f5c
X Y Z <rozbujnik@gmail.com>
2023-08-23 14:34:32 +0000

    Improved domain view

--- rsshistory/models/__init__.py
+++ rsshistory/models/__init__.py
@@ -1,11 +1,16 @@
 from .appmodels import ConfigurationEntry, UserConfig, PersistentInfo, BackgroundJob
 from .linkmodels import LinkDataModel, ArchiveLinkDataModel
-from .sourcemodels import SourceDataModel, SourceOperationalData
+from .sourcemodels import SourceDataModel, SourceOperationalData, SourceCategories, SourceSubCategories
 from .models import (
     RssSourceExportHistory,
     LinkTagsDataModel,
     LinkVoteDataModel,
     LinkCommentDataModel,
-    Domains,
     DataExport,
+)
+from .domains import (
+    Domains,
+    DomainsSuffixes,
+    DomainsTlds,
+    DomainsMains,
 )
