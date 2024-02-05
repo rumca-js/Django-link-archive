919e13f600a0af0a296380cea74d0b6c89967499
X Y Z <rozbujnik@gmail.com>
2024-02-05 07:07:51 +0000

    Added user entry transition history. Split entry detail page into several pieces.

---
+++ rsshistory/tests/test_userentryvisits.py
@@ -0,0 +1,35 @@
+from ..models import UserEntryVisits
+from ..controllers import LinkDataController, SourceDataController
+from .fakeinternet import FakeInternetTestCase
+
+
+class UserEntryVisitsTest(FakeInternetTestCase):
+    def setUp(self):
+        self.disable_web_pages()
+
+        ob = SourceDataController.objects.create(
+            url="https://youtube.com", title="YouTube", category="No", subcategory="No"
+        )
+
+        LinkDataController.objects.create(
+            source="https://youtube.com",
+            link="https://youtube.com?v=12345",
+            source_obj=ob,
+        )
+
+    def test_entry_visit(self):
+        entries = LinkDataController.objects.filter(link="https://youtube.com?v=12345")
+
+        UserEntryVisits.visited(entries[0], "test_username")
+
+        visits = EntryVisits.objects.filter(entry="https://youtube.com?v=12345")
+        self.assertEqual(visits.count(), 1)
+        self.assertEqual(visits[0].visits, 1)
+        self.assertEqual(visits[0].user, "test_username")
+
+        UserEntryVisits.visited(entries[0], "test_username")
+
+        visits = EntryVisits.objects.filter(entry="https://youtube.com?v=12345")
+        self.assertEqual(visits.count(), 1)
+        self.assertEqual(visits[0].visits, 2)
+        self.assertEqual(visits[0].user, "test_username")
