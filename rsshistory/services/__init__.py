6b0e363f975fbaa5fe536798c3b917f4e6e4f642
Rumpel Stilskin <renegat@renegat0x0.ddns.net>
2024-09-06 18:12:58 +0200

    Removed redundant directory

--- rsshistory/services/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Other site services
-"""
-from .translate import TranslateBuilder, GoogleTranslate
-from .internetarchive import InternetArchiveBuilder, InternetArchive
-from .waybackmachine import WaybackMachine
-from .validators import WhoIs, SchemaOrg, W3CValidator
-from .gitrepository import GitRepository
