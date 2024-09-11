11779cabc98a9f9213570c27ee77976608fe08c0
Rumpel Stilskin <renegat@renegat0x0.ddns.net>
2024-09-11 22:10:37 +0200

    Docker: celery and memcached

--- linklibrary/settings_template_postgres_celery.py
+++ linklibrary/settings_template_postgres_celery.py
@@ -158,6 +158,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
-        "LOCATION": "127.0.0.1:11211",
+        "LOCATION": "memcached:11211",
     }
 }
